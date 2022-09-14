2ea3a5a70464db8a38c8fbbd6d1ea1e702c144c4
Olivier Romanetti <olivier.romanetti@gmail.com>
2022-09-14 15:56:12 -0400

    basics 6

---
+++ 2-basics/2-better-code/app.js
@@ -0,0 +1,12 @@
+// ******************************************************
+// Créer un serveur Node
+// ******************************************************
+// Better code
+// ******************************************************
+
+const http = require('http');
+const routes = require('./routes');
+
+const server = http.createServer(routes);
+
+server.listen(3000);
